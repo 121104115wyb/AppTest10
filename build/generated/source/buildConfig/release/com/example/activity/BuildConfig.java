0f204dae591b12d8474471a3912ad9df5648dbf5
wangyubo <lishuwentimor1994@163.com>
2019-03-22 18:22:39 +0800

    first commit

---
+++ build/generated/source/buildConfig/release/com/example/activity/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.activity;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.activity";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.4";
+  // Fields from build type: release
+  public static final boolean LOG_DEBUG = false;
+}
